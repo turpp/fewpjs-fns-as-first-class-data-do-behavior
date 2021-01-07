ed78e37c6750c8db1ae07c3039941e79fe57600b
turpp <white.tripp@icloud.com>
2021-01-06 19:54:35 -0600

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,19 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(timeString) {
+  let time=parseInt(timeString.split(":")[0])
+  if (time < 12) {
+    return "Good Morning"}
+  else if (time > 17) {
+    return "Good Evening"}
+  else {
+    return "Good Afternoon"}
+}
+
 /* Write your implementation of displayMessage() */
+
+function displayMessage(string) {
+  let greeting = document.getElementById("greeting")
+  greeting.innerText = string
+}
